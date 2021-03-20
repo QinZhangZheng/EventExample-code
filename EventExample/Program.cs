a41bbdf5de9aa3d126076b2928d0a72e9b620da2
覃章政 <q308144069@163.com>
2021-03-20 22:22:37 +0800

    新建一个项目，里面创建了一个顾客类

---
+++ EventExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /// <summary>
+    /// 顾客类
+    /// </summary>
+    public class Customer
+    {
+
+    }
+}
